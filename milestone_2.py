a18ab3a1e08ba1ed9503d85e1a7c18899125c10f
Nicholas-Zambrano <nicholaszambrano@hotmail.co.uk>
2024-08-05 14:22:27 +0100

    added the first milestone

---
+++ milestone_2.py
@@ -0,0 +1,13 @@
+import random
+
+word_list = ["grapes","strawberries","blueberries","banana","apple"]
+
+word = random.choice(word_list)
+
+guess = input("Enter a single letter: ")
+
+if(len(guess) == 1 and guess.isalpha()):
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
+# print( word)
